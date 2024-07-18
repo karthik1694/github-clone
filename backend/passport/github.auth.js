8e20be00bf1d2185ae80a05bb7d6fe76223d5896
Karthik <karthikprem1694@gmail.com>
2024-07-18 22:16:51 +0530

    callback url changed

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -21,7 +21,7 @@ passport.use(
 		{
 			clientID: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET_KEY,
-			callbackURL: "/api/auth/github/callback",
+			callbackURL: "https://mern-github-prem.onrender.com/api/auth/github/callback",
 		},
 		async function (accessToken, refreshToken, profile, done) {
 			const user = await User.findOne({ username: profile.username });
